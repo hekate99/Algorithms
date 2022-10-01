23d5741024b200ba5e77c5f995ec9278606ff246
Surya Prakash Maurya <43824111+spm999@users.noreply.github.com>
2022-10-01 19:46:41 +0530

    Add files via upload

---
+++ vector_erase.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+// 	vector<int> myvector{ 1, 2, 3, 4, 5 };
+
+// 	auto it = myvector.end();
+// 	myvector.erase(it);
+
+// 	// Printing the Vector
+// 	for (auto x : myvector)
+// 		cout << x << " ";
+// 	return 0;
+// }
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int arr[2];
+   arr[3] = 10;  // Accessing out of bound
+   return 0;
+}
